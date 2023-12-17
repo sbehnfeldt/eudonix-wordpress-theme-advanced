2aa5b8e0168ef3629c050cc09829582075b0b90f
Stephen <stephen@behnfeldt.pro>
2023-12-17 12:16:40 -0500

    Lesson 10) HTML Template Part A Create new directory for the next WordPress theme, "Advanced".  Create static mock-up as starting point for the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# WordPress Theme "Advanced"
+Sing-along with Udemy Course __Learn to Create WordPress Theems by Buidling 10 Projects__
+by Samy Eduonix of Eduonix Learning Solutions, Eduonix-Tech, Samy Eduonix  
+_ref: https://www.udemy.com/course/learn-to-create-wordpress-themes-by-building-10-projects/_
+
+## Lesson 10) HTML Template Part A
+Static mockup for new theme project
---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+<header></header>
+<nav>
+    <ul>
+        <li><a href="#">One</a></li>
+        <li><a href="#">Two</a></li>
+        <li><a href="#">Three</a></li>
+    </ul>
+</nav>
+<main>
+    <article class="post">
+        <h2>Post 1</h2>
+        <p class="meta">Posted at 10:00 on May 9 by admin</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores blanditiis consectetur cum delectus
+            dolor eveniet, exercitationem id laudantium, maxime minima nihil numquam officia quos, ratione rerum
+            sapiente voluptas voluptates!</p>
+        <a href="#" class="button">Read More</a>
+    </article>
+
+    <article class="post">
+        <h2>Post 1</h2>
+        <p class="meta">Posted at 10:00 on May 9 by admin</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores blanditiis consectetur cum delectus
+            dolor eveniet, exercitationem id laudantium, maxime minima nihil numquam officia quos, ratione rerum
+            sapiente voluptas voluptates!</p>
+        <a href="#" class="button">Read More</a>
+    </article>
+
+    <article class="post">
+        <h2>Post 1</h2>
+        <p class="meta">Posted at 10:00 on May 9 by admin</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores blanditiis consectetur cum delectus
+            dolor eveniet, exercitationem id laudantium, maxime minima nihil numquam officia quos, ratione rerum
+            sapiente voluptas voluptates!</p>
+        <a href="#" class="button">Read More</a>
+    </article>
+</main>
+
+<footer>
+    <div class="copyright">
+        &copy;2016
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="#">One</a></li>
+            <li><a href="#">Two</a></li>
+            <li><a href="#">Three</a></li>
+        </ul>
+    </div>
+</footer>
+
+</body>
+</html>
